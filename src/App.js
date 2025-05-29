7dc5ed6fc7d077bfaa26def71e2e978787331711
Adam <experimental@example.com>
2025-05-29 23:24:26 +0500

    decompose component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./index.scss";
 
+const Modal = ({ toggleModal, setToggleModal, children }) => (
+  <div className={`overlay animated ${toggleModal ? "show" : ""}`}>
+    <div className="modal">
+      <svg
+        onClick={() => {
+          setToggleModal(!toggleModal);
+        }}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children}
+    </div>
+  </div>
+);
+
 function App() {
   const [toggleModal, setToggleModal] = React.useState(false);
 
-  const toggle = () => {
-    setToggleModal(!toggleModal);
-  };
-
   return (
     <div className="App">
-      <button onClick={toggle} className="open-modal-btn">
+      <button
+        onClick={() => {
+          setToggleModal(!toggleModal);
+        }}
+        className="open-modal-btn"
+      >
         ✨ Открыть окно
       </button>
-      <div className={`overlay animated ${toggleModal ? "show" : ""}`}>
-        <div className="modal">
-          <svg onClick={toggle} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div>
+      <Modal toggleModal={toggleModal} setToggleModal={setToggleModal}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>hello</h3>
+      </Modal>
     </div>
   );
 }
